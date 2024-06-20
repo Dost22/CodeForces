c67a79c8e7e06a07b590df93609e72d67130af95
Dost22 <94752376+Dost22@users.noreply.github.com>
2024-06-20 20:42:20 +0300

    Mono

---
+++ MISC/monostack.cpp
@@ -0,0 +1,16 @@
+void compute(const vi& a, vi& L,vi& R) {
+    //NEREYE KADAR EN BÜYÜK (EŞİTLERİ SOLA KADAR ALIYOR)
+    int n = a.size()-1;
+    stack<pii> st;
+    for (int i=1;i<=n;i++) {
+        while (!st.empty() && st.top().ff <= a[i]) st.pop();
+        L[i] = st.empty()?1:st.top().ss+1;
+        st.push({a[i],i});
+    }
+    while (!st.empty()) st.pop();
+    for (int i=n;i>=1;i--) {
+        while (!st.empty() && st.top().ff < a[i]) st.pop();
+        R[i] = st.empty()?n:st.top().ss-1;
+        st.push({a[i],i});
+    }
+}
--- core.cpp
+++ core.cpp
@@ -9,3 +9,18 @@ using namespace std;
 #define sp << " " << 
 #define vi vector<int>
 const int N = 2e5+1,MOD = 998244353;
+
+void solve() {
+
+}
+
+signed main() { 
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    #ifdef Dodi
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    int t = 1;
+    cin >> t;
+    while (t --> 0) solve();
+}
